b8ff510feaeb7129b56606eaa8fa6c4c0ec02408
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-01-21 10:48:14 +0200

    add about page

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import express from "express";
-import handelbars from "express-handlebars";
+import express from 'express';
+import handelbars from 'express-handlebars';
 
 const app = express();
 
 //set the view engine to handlebars
-app.engine("hbs", handelbars.engine({
-    extname: "hbs"
+app.engine('hbs', handelbars.engine({
+    extname: 'hbs'
 }));
 
-app.set("view engine", "hbs");
-app.set("views", "./src/views");
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
 
-app.use('/static', express.static("src/public"));
+app.use('/static', express.static('src/public'));
 
 //render the home page
-app.get("/", (req, res) => {
-    res.render("home");
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
 //render the about page
-app.get("/about", (req, res) => {  
-    res.render("about", { layout: false });
+app.get('/about', (req, res) => {  
+    res.render('about');
 });
 
 app.listen(3000, () => {
-    console.log("Server is listening on http://localhost:3000");
+    console.log('Server is listening on http://localhost:3000');
 });
