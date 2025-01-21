4a347c7358381cf3d470c9ad4ba05a6bc0f2f961
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-01-21 09:24:46 +0200

    add and setup handlebars

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import handelbars from "express-handlebars";
 
 const app = express();
 
+//set the view engine to handlebars
+app.engine("hbs", handelbars.engine({
+    extname: "hbs"
+}));
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
+
 app.get("/", (req, res) => {
-    res.send("It Works!");
-    });
+    res.render("home", { layout: false });
+});
 
 app.listen(3000, () => {
     console.log("Server is listening on http://localhost:3000");
-    });
+});
