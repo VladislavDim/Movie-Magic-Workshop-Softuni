271d6388fed557488ceaf43b859a2392778dc0b8
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-01-22 09:11:08 +0200

    add movie controller and add page

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+
 import homeController from './controllers/home-controller.js';
+import movieController from './controllers/movie-controller.js';
 
 const routes = Router();
 
 routes.use(homeController);
+routes.use(movieController);
 
 //render the not found page
 routes.get('*', (req, res) => {
