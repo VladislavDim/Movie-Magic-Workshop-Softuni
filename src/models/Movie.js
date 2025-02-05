76cf080d408102161d1364092da711af969792af
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-02-05 08:42:22 +0200

    Add movie model validation

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -2,14 +2,50 @@ import { Schema, model, Types } from 'mongoose';
 
 //create a schema
 const movieSchema = new Schema({
-    title: String,
+    title: {
+        type: String,
+        required: [true, 'Title is required!'],
+        minLength: [5, 'The movie title must be at least 5 characters long!'],
+        maxLength: [250, 'The movie title can\'t be more than 250 characters long!'],
+        match: [/^[a-zA-Z0-9\s]+$/, 'Title should contain only English letters, digits, and whitespaces.']
+    },
     category: String,
-    genre: String,
-    director: String,
-    year: Number,
-    imageUrl: String,
-    rating: Number,
-    description: String,
+    genre: {
+        type: String,
+        required: [true, 'Genre is required!'],
+        minLength: [5, 'The movie genre must be at least 5 characters long!'],
+        maxLength: [250, 'The movie genre can\'t be more than 250 characters long!'],
+        match: [/^[a-zA-Z0-9\s]+$/, 'Genre should contain only English letters, digits, and whitespaces.']
+    },
+    director: {
+        type: String,
+        required: [true, 'Director is required!'],
+        minLength: [5, 'The movie director must be at least 5 characters long!'],
+        maxLength: [250, 'The movie director can\'t be more than 250 characters long!'],
+        match: [/^[a-zA-Z0-9\s]+$/, 'Director\'s name should contain only English letters, digits, and whitespaces.']
+    },
+    year: {
+        type: Number,
+        required: [true, 'Year is required!'],
+        min: [1900, 'Year should not be earlier than 1900.'],
+        max: [2025, 'Year should not be later than 2025.']
+    },
+    imageUrl: {
+        type: String,
+        match: [/^(https?:\/\/).+\.(jpg|jpeg|png|gif|webp)$/i,'Image URL should start with \'http://\' or \'https://\' and end with a valid image format (jpg, jpeg, png, gif, webp).']
+    },
+    rating: {
+        type: Number,
+        required: [true, 'Rating is required!'],
+        min: 1,
+        max: 10
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required!'],
+        minLength: 20,
+        match: [/^[a-zA-Z0-9\s]+$/, 'Description should contain only English letters, digits, and whitespaces.']
+    },
     casts: [{
         //_id: false,
         role: String,
