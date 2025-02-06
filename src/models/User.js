81adb5c598c571b513028101d3997dc3bad59b10
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-02-06 11:51:30 +0200

    Add user validation

--- src/models/User.js
+++ src/models/User.js
@@ -2,8 +2,17 @@ import { Schema, model } from "mongoose";
 import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
-    email: String,
-    password: String
+    email: {
+        type: String,
+        unique: true,
+        match: /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/,
+        minLength: 10
+    },
+    password: {
+        type: String,
+        match: /^[A-Za-z0-9]+$/,
+        minLength: 6
+    }
 });
 
 userSchema.pre('save', async function () {
