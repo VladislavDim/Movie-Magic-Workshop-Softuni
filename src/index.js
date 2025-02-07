4cc94c380e0967cc774071545d34943f1b4d8e8b
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-02-07 22:29:38 +0200

    Add temp data middleware

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import handelbars from 'express-handlebars';
+import expressSession from 'express-session';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import 'dotenv/config';
@@ -8,6 +9,7 @@ import routes from './routes.js';
 import showRatingHelper from './helpers/ratingHelper.js';
 import ifCondHelper from './helpers/ifCondHelper.js';
 import { authMiddleware } from './middlewares/auth-middleware.js';
+import { tempData } from './middlewares/temp-data-middleware.js';
 
 const app = express();
 
@@ -39,6 +41,16 @@ app.set('views', './src/views');
 app.use('/static', express.static('src/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(expressSession({
+    secret: "asd2312ft$%3df3242ds3",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        httpOnly: true
+    }
+}));
+app.use(tempData);
 app.use(authMiddleware());
 
 app.use(routes);
